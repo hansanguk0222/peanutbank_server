90ad82336f0b332502aad21302074631c3d0f7dd
Hansanguk <hansanguk0222@gmail.com>
2021-07-05 19:48:00 +0900

    [feat] swagger 세팅 추가

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
-import "module-alias/register";
-import express from "express";
-import dotenv from "dotenv";
-import path from "path";
-import { connect, disconnect } from "@/src/db";
+import 'module-alias/register';
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { connect, disconnect } from '@/src/db';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import http from 'http';
 
 dotenv.config({
-  path: path.resolve(
-    process.cwd(),
-    process.env.NODE_ENV == "production" ? ".env" : ".env.dev"
-  ),
+  path: path.resolve(process.cwd(), process.env.NODE_ENV == 'production' ? '.env' : '.env.dev'),
 });
 
+const port = process.env.PORT || 8080;
+
+const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'));
+
 const app = express();
+const server = http.createServer(app);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 connect();
+
+server.listen(port, () => {
+  console.log(`${port}에서 열림`);
+});

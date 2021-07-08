b0f51c81e9f15fee32d19159b4e4b7c8ebd02ef6
Hansanguk <hansanguk0222@gmail.com>
2021-07-08 21:58:14 +0900

    [refactor] 모델을 반환하지 않아서 모델을 반환하는 부분을 추가

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import { AccountbookModel, CategoryModel, LedgerModel, UserModel } from '@/src/models';
 export const connect = () => {
   console.log(process.env.MONGO_URL);
   const uri = `${process.env.MONGO_URL}`;
@@ -17,6 +17,12 @@ export const connect = () => {
     .catch((err) => {
       console.log(err);
     });
+  return {
+    AccountbookModel,
+    CategoryModel,
+    LedgerModel,
+    UserModel,
+  };
 };
 
 export const disconnect = () => {

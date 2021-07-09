b1e7a1c75b548d33c7bf7cfd0618e43c3599a9fc
Hansanguk <hansanguk0222@gmail.com>
2021-07-09 10:45:08 +0900

    [refactor] export를 객체 자체로 변경

--- src/models/User.models.ts
+++ src/models/User.models.ts
@@ -1,5 +1,5 @@
 import { model } from 'mongoose';
 import { IUserDocument, IUserModel } from '@/src/types';
-import UserSchema from '@/src/schemas/User.schemas';
+import { UserSchema } from '@/src/schemas/User.schemas';
 
 export const UserModel = model<IUserDocument>('user', UserSchema) as IUserModel;

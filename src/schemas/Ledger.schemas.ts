7e875f1b79c92941e11f22601fe96f8e773e631f
Hansanguk <hansanguk0222@gmail.com>
2021-07-09 10:44:41 +0900

    [refactor] _id 삭제

--- src/schemas/Ledger.schemas.ts
+++ src/schemas/Ledger.schemas.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 export const LedgerSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   discription: String,
   categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'categorySchema' },
   amount: Number,

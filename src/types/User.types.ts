b93c5c8a1388ae016b0f21f33154198b1602ea44
Hansanguk <hansanguk0222@gmail.com>
2021-07-11 20:54:55 +0900

    [feat] 유저 모델에 함수를 추가하여 해당 함수의 타입을 추가

--- src/types/User.types.ts
+++ src/types/User.types.ts
@@ -1,13 +1,14 @@
 import { Document, Model, ObjectId } from 'mongoose';
 import { IAccountBook } from './Accountbook.types';
 import { ICategory } from './Category.types';
+import mongoose from 'mongoose';
 
 export interface IUser {
   userId: string;
   OAuthType: string;
   nickname: string;
   image: string;
-  accountBook: [IAccountBook];
+  accountbooks: [IAccountBook];
   categories: [ICategory];
   createdAt: Date;
   updatedAt: Date;
@@ -15,9 +16,29 @@ export interface IUser {
 
 export interface IUserDocument extends IUser, Document {
   updateImage: (this: IUserDocument, userId: string, image: string) => Promise<string>;
+  updateNickname: (this: IUserDocument, { nickname }: { nickname: string }) => Promise<void>;
+  updateCategoryColor: (this: IUserDocument, { categoryId, color }: { categoryId: string; color: string }) => Promise<void>;
+  deleteCategory: (this: IUserDocument, { categoryId }: { categoryId: string }) => Promise<void>;
+  findAccountbookByYYYYMM: (this: IUserDocument, { yyyy, mm }: { yyyy: string; mm: string }) => Promise<IAccountBook>;
+  createLedger: (
+    this: IUserModel,
+    {
+      nickname,
+      yyyy,
+      mm,
+      dd,
+      incomeOrExpenditure,
+      description,
+      amount,
+      categoryId,
+    }: { nickname: string; yyyy: string; mm: string; dd: string; incomeOrExpenditure: string; description: string; amount: number; categoryId: string }
+  ) => Promise<mongoose.Types.ObjectId>;
 }
 
 export interface IUserModel extends Model<IUserDocument> {
   findByUserIdOrCreateUser: (this: IUserModel, userId: string, OAuthType: string) => Promise<IUserDocument>;
-  findCategoriesByUserId: (this: IUserModel, userId: string, OAuthType: string) => Promise<[ICategory]>;
+  findCategoriesByNickname: (this: IUserModel, nickname: string) => Promise<[ICategory]>;
+  createCategory: (this: IUserModel, { nickname, name, color }: { nickname: string; name: string; color: string }) => Promise<mongoose.Types.ObjectId>;
+  findUserByNickname: (this: IUserModel, { nickname }: { nickname: string }) => Promise<IUserDocument>;
+  findCategoryByNicknameAndCategoryId: (this: IUserModel, nickname: string, categoryId: string) => Promise<IUserDocument>;
 }

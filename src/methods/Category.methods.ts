ce15503a2ef9db6799fbd7f6222342c7b0b4f40f
Hansanguk <hansanguk0222@gmail.com>
2021-07-14 11:07:30 +0900

    [chore] 임시 커밋

--- src/methods/Category.methods.ts
+++ src/methods/Category.methods.ts
@@ -1,12 +1,14 @@
 import { Document } from 'mongoose';
 import { ICategoryDocument } from '@/src/types';
 
-export async function updateCategoryColor(this: ICategoryDocument, color: string): Promise<void> {
-  this.color = color;
-  return;
-}
+// export async function updateCategoryColor(this: ICategoryDocument, color: string): Promise<void> {
+//   this.color = color;
+//   await this.save();
+//   return;
+// }
 
-export async function deleteCategory(this: ICategoryDocument): Promise<void> {
-  this.isExist = false;
-  return;
-}
+// export async function deleteCategory(this: ICategoryDocument): Promise<void> {
+//   this.isExist = false;
+//   await this.save();
+//   return;
+// }

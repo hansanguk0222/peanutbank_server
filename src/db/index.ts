d05ae0b2db0a4d7dee50805fa2217e1f580df46e
Hansanguk <hansanguk0222@gmail.com>
2021-07-14 11:06:34 +0900

    [chore] 콘솔로그 제거

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { AccountbookModel, CategoryModel, LedgerModel, UserModel } from '@/src/models';
+
 export const connect = () => {
-  console.log(process.env.MONGO_URL);
   const uri = `${process.env.MONGO_URL}`;
 
   mongoose

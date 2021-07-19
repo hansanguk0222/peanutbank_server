7d2d856464cd5f4b49dc4ebc7580068a55107f20
Hansanguk <hansanguk0222@gmail.com>
2021-07-19 22:49:11 +0900

    [feat] 구글 로그인 관련 로직 작성

---
+++ src/routes/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { verifyRequestData } from '@/src/utils/utilFunc';
+import { Request, Response, NextFunction } from 'express';
+import { findUserByUserIdService } from '@/src/services';
+import { TIME } from '@/src/utils/constants';
+
+export const googleLogin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const { googleId } = req.body;
+  if (verifyRequestData([googleId])) {
+    try {
+      const { accessToken, image, nickname, oauthType } = await findUserByUserIdService({ userId: googleId, oauthType: 'google' });
+      const user = { nickname, image, oauthType };
+      res.cookie('accessToken', accessToken, {
+        httpOnly: true,
+        maxAge: TIME.FIVE_MINUTE,
+      });
+      res.status(200).json({ user });
+      return;
+    } catch (err) {
+      next(err);
+      return;
+    }
+  }
+  res.status(401).end();
+  return;
+};
